0fda7f5aa6cf3f94817e73cbe99586e092a4f2c5
NuclearGamerYT <166153016+NuclearGamerYT@users.noreply.github.com>
2025-07-04 13:44:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Historia',
+    'Em que ano começou a Segunda Guerra Mundial?',
+    'Foi em 01 de setembro de 1939'
+)
+
+criaCartao(
+    'Historia',
+    'Em que ano terminou a Segunda Guerra Mundial?',
+    'Foi em 02 de setembro de 1945'
+)
+
+criaCartao(
+    'Historia',
+    'quem era os aliados na Segunda Guerra Mundial?',
+    'Os aliados eram: Estados Unidos, Reino Unido, União Soviética, China e França'
+)
+
+criaCartao(
+    'Historia',
+    'quem era os eixos na Segunda Guerra Mundial?',
+    'Os eixos eram: Alemanha, Itália e Japão'
+)
